3e1acc2373c9e7a3e0e94aed11e7dd310f0d3284
chenghao <hao-bupt@163.com>
2019-03-19 19:27:14 +0800

    add utils

---
+++ src/main/java/com/threewater/webserver/webtemplate/util/HttpClientUtil.java
@@ -0,0 +1,88 @@
+package com.threewater.webserver.webtemplate.util;
+
+import com.threewater.webserver.webtemplate.exception.CommonException;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.nio.charset.Charset;
+
+public class HttpClientUtil {
+
+    public static String httpSendPost(String url, String json){
+        return httpSendPost(url, json, Charset.forName("utf-8"));
+    }
+
+    public static String httpSendPost(String url, String json, Charset charset){
+        String result = "";
+        try {
+            // 创建httpclient对象
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+
+            // 创建post方式请求对象
+            HttpPost httpPost = new HttpPost(url);
+
+            // 设置参数到请求对象中
+            StringEntity stringEntity = new StringEntity(json, ContentType.APPLICATION_JSON);
+            stringEntity.setContentEncoding(charset.toString());
+            httpPost.setEntity(stringEntity);
+
+            // 执行请求操作，并拿到结果（同步阻塞）
+            CloseableHttpResponse response = httpClient.execute(httpPost);
+
+            // 获取结果实体
+            // 判断网络连接状态码是否正常(0--200都数正常)
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                result = EntityUtils.toString(response.getEntity(), "utf-8");
+            }
+            // 释放链接
+            response.close();
+        }catch (Exception e){
+            throw new CommonException("TWFT0003", "POSTq请求发送失败", e);
+        }
+
+        return result;
+    }
+
+    public static String httpSendGet(String url){
+        return httpSendGet(url, Charset.forName("utf-8"));
+    }
+
+    public static String httpSendGet(String url, Charset charset){
+        String result = "";
+
+        try {
+            // 创建httpclient对象
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+
+            // 创建get方式请求对象
+            HttpGet httpGet = new HttpGet(url);
+            httpGet.addHeader("Content-type", "application/json");
+            // 通过请求对象获取响应对象
+            CloseableHttpResponse response = httpClient.execute(httpGet);
+
+            // 获取结果实体
+            // 判断网络连接状态码是否正常(0--200都数正常)
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                result = EntityUtils.toString(response.getEntity(), "utf-8");
+            }
+            // 释放链接
+            response.close();
+        }catch(Exception e){
+            throw new CommonException("TWFT0003", "GET请求发送失败", e);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args){
+        String s = httpSendGet("http://www.baidu.com");
+        System.out.println(s);
+    }
+}
---
+++ src/main/java/com/threewater/webserver/webtemplate/util/JwtUtil.java
@@ -0,0 +1,82 @@
+package com.threewater.webserver.webtemplate.util;
+
+import com.threewater.webserver.webtemplate.exception.CommonException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+//@ConfigurationProperties(prefix = "custom.jwt")
+public class JwtUtil {
+    private static final Logger log = LoggerFactory.getLogger(JwtUtil.class);
+
+    @Value("custom.jwt.secret:secret-three")
+    private String secret;
+    @Value("custom.jwt.expire:3600")
+    private long expire;
+
+    /**
+     * 生成jwt token
+     */
+    public String generateToken(Map<String, ?> infoMap) {
+        Date nowDate = new Date();
+        //过期时间
+        Date expireDate = new Date(nowDate.getTime() + expire * 1000);
+
+        Map<String, Object> map = new HashMap();
+
+        map.put("data", infoMap);
+        String userId = (String)infoMap.get("userId");
+
+        return "Bearer " + Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .setClaims(map)//一定要放在最前面
+                .setSubject(userId)
+                .setIssuedAt(nowDate)
+                .setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    public Claims getClaimByToken(String token) {
+        Claims body = null;
+        try {
+            body = Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token.replace("Bearer ",""))
+                    .getBody();
+        }catch (Exception e){
+            log.debug("validate is token error ", e);
+            throw new CommonException("TWFT0003", "token解析异常", e);
+        }
+        return body;
+    }
+
+    /**
+     * token是否过期
+     * @return  true：过期
+     */
+    public boolean isTokenExpired(Date expiration) {
+        return expiration.before(new Date());
+    }
+
+    public static void main(String[] args){
+        JwtUtil jwt = new JwtUtil();
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", "testUser");
+        map.put("testData", "data");
+        String str = jwt.generateToken(map);
+        System.out.println(str);
+        System.out.println(jwt.getClaimByToken(str));
+        System.out.println(jwt.isTokenExpired(jwt.getClaimByToken(str).getExpiration()));
+    }
+
+}
