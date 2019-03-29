466b5f87785a3ce6d9c77296758f6ce076d67388
chenghao <hao-bupt@163.com>
2019-03-29 15:48:15 +0800

    add models

--- src/main/java/com/threewater/webserver/webtemplate/po/LogInfo.java
+++
@@ -1,8 +0,0 @@
-package com.threewater.webserver.webtemplate.po;
-
-public class LogInfo {
-    private String status;
-    private String code;
-    private String msg;
-    private String data;
-}
